40ce3d72fef32640beba45d91deea340f561bd73
Ravi Shankar <129148887+Ravi-Technical@users.noreply.github.com>
2026-04-09 14:44:00 +0530

    Add debugger to uploadImage method
    Added a debugger statement in the uploadImage method to assist with debugging image uploads.

--- frontend/src/app/@core/seller.service.ts
+++ frontend/src/app/@core/seller.service.ts
@@ -25,6 +25,7 @@ export class SellerService {
 
   //************************************ Cloudinary Product Upload Configuration ****************************************//
   uploadImage(file: File) {
+    debugger;
   const formData = new FormData();
   formData.append('file', file);
   // REQUIRED
